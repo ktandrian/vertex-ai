28eb55ac917101b62e86fc00aec47a30e86e7c8c
Kenrick Tandrian <166034667+ktandrian@users.noreply.github.com>
2024-06-17 18:28:06 +0700

    feat(scripts): add script to list reasoning engines

---
+++ scripts/list_reasoning_engines.py
@@ -0,0 +1,10 @@
+from decouple import config
+from vertexai.preview import reasoning_engines
+import vertexai
+
+PROJECT_ID = config("PROJECT_ID", default="YOUR_PROJECT_ID")
+LOCATION = "us-central1" # Only available in us-central1 as per 2024-06-17.
+
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+reasoning_engine_list = reasoning_engines.ReasoningEngine.list()
+print(reasoning_engine_list)
